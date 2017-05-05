661dd0ca0d86cfeb951fac806ab5e94afc38d088
Chris Dostert <chris.dostert@nintex.com>
2017-05-05 10:38:30 -0700

    chore) initial commit

---
+++ suite_test.go
@@ -0,0 +1,13 @@
+package vgit
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"testing"
+)
+
+func Test(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "virtual-go/gopkg.in-src-d-go-git.v4")
+}
